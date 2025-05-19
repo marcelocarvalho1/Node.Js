b02d688350f52716d00b8c06476288f867740d53
Marcelo <marceloo.dev@gmail.com>
2025-05-19 20:38:14 -0300

    Refactor user routes to use async/await and improve error handling

--- modules/express.js
+++ modules/express.js
@@ -5,20 +5,31 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-  res.contentType("text/html");
-  res.status(200).send("<h1>Hello, world</h1>");
-});
-
-app.get("/users", (req, res) => {
+//pegar todos os useres
+app.get("/users", async (req, res) => {
   try{
+    const users = await UserModel.find({});
 
+    res.status(200).json(users);
   } catch (error){
+    res.status(500).send(error.message);
 
   }
-  res.status(200).json(users);
-});
 
+});
+//pegar o user por id
+app.get("/users/:id", async (req, res) => {
+  try{
+    const id = req.params.id;
+
+    const user = await UserModel.findById(id)
+
+    return res.status(200).json(user)
+  }catch(error){
+         res.status(500).send(error.message);
+  }
+})
+//criar user
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
